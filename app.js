d0d264a809d86349a57e5c79f2b5b64eb2d216ad
Abhishek-1839 <abhishektomarsingh7@gmail.com>
2024-08-31 18:50:53 +0530

    evening 2nd day

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
+// import the express module
 const express = require('express');
 const userRouter = require('./routes/userRoutes');
+const authRouter = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const bookRouter = require('./routes/bookRoutes');
 
 // create a new express app
 const app = express();
 
+// use the express json middleware for parsing json data
+app.use(express.json());
+
+// use the cookie parser middleware
+app.use(cookieParser());
+
 // use the routers
-app.use('/api/v1/admin/users', userRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/user', userRouter);
+app.use('/api/v1/admin/books', bookRouter);
 
 // export the app
 module.exports = app;
